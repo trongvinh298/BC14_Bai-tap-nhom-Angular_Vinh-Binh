ba0fb372cddefe1b6efc936bf81950245aec2031
Nguyen Quoc Binh <85874078+Quocbinh7416@users.noreply.github.com>
2021-11-21 19:56:09 +0700

    update 21/11/2021

--- src/app/pages/home-template/home/home.module.ts
+++ src/app/pages/home-template/home/home.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { ChildHomeComponent } from './child-home/child-home.component';
 import { FormsModule } from '@angular/forms';
 import { ShareModuleModule } from 'src/app/_core/shares/share-module/share-module.module';
 import { MaterialModule } from 'src/app/_core/shares/share-module/material-module';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
-  declarations: [HomeComponent, ChildHomeComponent],
+  declarations: [HomeComponent, CourseComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
